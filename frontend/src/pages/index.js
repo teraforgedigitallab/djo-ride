12c36739a9a7bb7dba0e4c350557560ea060b55b
Teraforge Digital Lab <teraforgedigitallab@gmail.com>
2025-08-09 01:16:06 +0530

    Delete frontend directory

--- frontend/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from './Home'
-import Login from './Login'
-import SignUp from './SignUp'
-import Book from './Book'
-import Admin from './Admin'
-
-export { Book ,Login, SignUp, Home, Admin }
